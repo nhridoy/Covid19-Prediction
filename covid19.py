202b32c7357c042451f69714ff7351c08526df24
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2021-04-22 00:58:05 +0600

    Improved

--- covid19.py
+++ covid19.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from covid import Covid
 import geocoder
 import ipinfo
+import datetime
 
 
 ### Geting Location ###
@@ -27,6 +28,11 @@ st.title(
     """
 )
 
+dt = datetime.datetime.now()
+date = dt.date()
+time = dt.time()
+st.write(f"Date: {date.strftime('%d - %B - %Y')}, Time: {time.strftime('%I:%M %p')}")
+
 ### Getting worldometer data ###
 covid = Covid(source="worldometers")
 
@@ -48,25 +54,36 @@ country = pd.DataFrame(countries, columns=["country"])
 
 for i in range(country["country"].count()):
     if country["country"][i] == county_name.lower():
-        st.write(f"Location Detected {country['country'][i].upper()}")
+        st.sidebar.write(f"Location Detected {country['country'][i].upper()}")
         break
     else:
         i = 0
 if i == 0:
-    st.write("Location Detection Error! Please Select from Below.")
+    st.sidebar.write("Location Detection Error! Please Select from Below.")
 
-st.subheader("Select Country")
+st.sidebar.subheader("Select Country")
 # st.write(country)
-selected_country = st.selectbox("Country:", country, index=i)
+selected_country = st.sidebar.selectbox("Country:", country, index=i)
 
 st.write(f"You selected {selected_country.upper()}")
 
 world_total_confirmed_cases = covid.get_total_confirmed_cases()
+world_total_death = covid.get_total_deaths()
+world_total_recovered = covid.get_total_recovered()
+world_total_active = covid.get_total_active_cases()
 
-st.write(f"Total Cases Worldwide: {world_total_confirmed_cases}")
 
 country_cases = covid.get_status_by_country_name(selected_country)
-st.write(f"Total Cases in {selected_country.upper()}: {country_cases['confirmed']}")
+country_total_cases = country_cases['confirmed']
+country_total_death = country_cases['deaths']
+country_total_recovered = country_cases['recovered']
+country_total_active = country_cases['active']
+country_cases_new = country_cases['new_cases']
+country_death_new = country_cases['new_deaths']
+
+# st.write(f"Total Cases Worldwide: {world_total_confirmed_cases}")
+
+# st.write(f"Total Cases in {selected_country.upper()}: {country_cases['confirmed']}")
 
 st.write('''
         <style>
@@ -95,24 +112,55 @@ st.write('''
 			}
 		</style>
 ''', unsafe_allow_html=True)
+
 st.write(f'''
         <div class="container">
 			<section class="box">
-				<h2>This is header</h2>
+				<h3>Total Recovered WorldWide</h3>
+				<p>{world_total_recovered}</p>
 			</section>
 			<section class="big-box box">
-				<h2>Total Cases WorldWide</h2>
+				<h3>Total Cases WorldWide</h3>
 				<p>{world_total_confirmed_cases}</p>
 			</section>
 			<section class="big-box box">
-				<h2>Total Cases in {selected_country.upper()}</h2>
-				<p>{country_cases["confirmed"]}</p>
+				<h3>Total Death WorldWide</h3>
+				<p>{world_total_death}</p>
 			</section>
 			<section class="box">
-				<h2>This is header</h2>
+				<h3>Total Active WorldWide</h3>
+				<p>{world_total_active}</p>
 			</section>
 		</div>
 ''', unsafe_allow_html=True)
+
+
+
+
+st.write(f'''
+        <div class="container">
+			<section class="box">
+				<h3>Total Recovered {selected_country.upper()}</h3>
+				<p>{country_total_recovered}</p>
+			</section>
+			<section class="big-box box">
+				<h3>Total Cases {selected_country.upper()}</h3>
+				<p>{country_total_cases}</p>
+				<p>New Cases: {country_cases_new}</p>
+			</section>
+			<section class="big-box box">
+				<h3>Total Death {selected_country.upper()}</h3>
+				<p>{country_total_death}</p>
+				<p>New Deaths: {country_death_new}</p>
+			</section>
+			<section class="box">
+				<h3>Total Active {selected_country.upper()}</h3>
+				<p>{country_total_active}</p>
+			</section>
+		</div>
+''', unsafe_allow_html=True)
+
+
 st.markdown(
     f"""
 <style>
